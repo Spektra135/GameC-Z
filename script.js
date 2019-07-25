a2489673a9a3089a1181bbb16a3e3fee59171b0b
Spektra135 <spektra135@gmail.com>
2019-07-25 20:32:04 +0300

    Finish ugly

--- script.js
+++ script.js
@@ -24,7 +24,36 @@ function checkEnd() {
   if (fieldsList[0] && fieldsList[1] && fieldsList[2] && fieldsList[3] && fieldsList[4] && fieldsList[5] && fieldsList[6] && fieldsList[7] && fieldsList[8]) return true;
 }
 
+function checkWin() {
+  if (fieldsList[0] == 'player' && fieldsList[1] == 'player' && fieldsList[2] == 'player') return true;
+  if (fieldsList[3] == 'player' && fieldsList[4] == 'player' && fieldsList[5] == 'player') return true;
+  if (fieldsList[6] == 'player' && fieldsList[7] == 'player' && fieldsList[8] == 'player') return true;
+  if (fieldsList[0] == 'player' && fieldsList[3] == 'player' && fieldsList[6] == 'player') return true;
+  if (fieldsList[1] == 'player' && fieldsList[4] == 'player' && fieldsList[7] == 'player') return true;
+  if (fieldsList[2] == 'player' && fieldsList[5] == 'player' && fieldsList[8] == 'player') return true;
+  if (fieldsList[0] == 'player' && fieldsList[4] == 'player' && fieldsList[8] == 'player') return true;
+  if (fieldsList[2] == 'player' && fieldsList[4] == 'player' && fieldsList[6] == 'player') return true;
+  if (fieldsList[0] == 'ai' && fieldsList[1] == 'ai' && fieldsList[2] == 'ai') return false;
+  if (fieldsList[3] == 'ai' && fieldsList[4] == 'ai' && fieldsList[5] == 'ai') return false;
+  if (fieldsList[6] == 'ai' && fieldsList[7] == 'ai' && fieldsList[8] == 'ai') return false;
+  if (fieldsList[0] == 'ai' && fieldsList[3] == 'ai' && fieldsList[6] == 'ai') return false;
+  if (fieldsList[1] == 'ai' && fieldsList[4] == 'ai' && fieldsList[7] == 'ai') return false;
+  if (fieldsList[2] == 'ai' && fieldsList[5] == 'ai' && fieldsList[8] == 'ai') return false;
+  if (fieldsList[0] == 'ai' && fieldsList[4] == 'ai' && fieldsList[8] == 'ai') return false;
+  if (fieldsList[2] == 'ai' && fieldsList[4] == 'ai' && fieldsList[6] == 'ai') return false;
+  return;
+}
+
+function returnResult() {
+  if (checkWin() === true) {
+    alert( 'Вы выиграли');
+  } else if (checkWin() === false){
+    alert( 'Вы проиграли' );
+  }
+}
+
 function reset() {
+  returnResult();
   alert ('Игра окончена');
   location.reload();
 }
