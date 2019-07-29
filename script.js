a50bcb52c84cac6dff0ef84cb241425281bc2427
Arthur Creek <arthur.creek@gmail.com>
2019-07-29 23:36:31 +0300

    return and comment deleted lines

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function move(id, role) {
   if (fieldsList[id]) return false;
   fieldsList[id] = role;
   document.getElementById(id).className = 'cell ' + role;
+  // !checkEnd() ? (role == 'player')  ? ai() : null : reset();
   var result = checkEnd();
   if(result) {
     setTimeout(function () {
@@ -40,6 +41,7 @@ function checkEnd() {
     var cell3 = fieldsList[indexes[2]];
     for (var j = 0; j < roles.length; j++) {
       if (cell1 === roles[j] && cell3 === roles[j] && cell2 === roles[j]) {
+        // return true;
         return roles[j];
       }
     }
@@ -55,10 +57,44 @@ function checkEnd() {
   for (var i = 0; i < fieldsList.length; i++) {
     allFields = allFields && !!fieldsList[i];
   }
+  // return allFields;
   return allFields
     ? 'nobody'
     : false;
 }
+
+// function checkWin() {
+//   if (fieldsList[0] == 'player' && fieldsList[1] == 'player' && fieldsList[2] == 'player') return true;
+//   if (fieldsList[3] == 'player' && fieldsList[4] == 'player' && fieldsList[5] == 'player') return true;
+//   if (fieldsList[6] == 'player' && fieldsList[7] == 'player' && fieldsList[8] == 'player') return true;
+//   if (fieldsList[0] == 'player' && fieldsList[3] == 'player' && fieldsList[6] == 'player') return true;
+//   if (fieldsList[1] == 'player' && fieldsList[4] == 'player' && fieldsList[7] == 'player') return true;
+//   if (fieldsList[2] == 'player' && fieldsList[5] == 'player' && fieldsList[8] == 'player') return true;
+//   if (fieldsList[0] == 'player' && fieldsList[4] == 'player' && fieldsList[8] == 'player') return true;
+//   if (fieldsList[2] == 'player' && fieldsList[4] == 'player' && fieldsList[6] == 'player') return true;
+//   if (fieldsList[0] == 'ai' && fieldsList[1] == 'ai' && fieldsList[2] == 'ai') return false;
+//   if (fieldsList[3] == 'ai' && fieldsList[4] == 'ai' && fieldsList[5] == 'ai') return false;
+//   if (fieldsList[6] == 'ai' && fieldsList[7] == 'ai' && fieldsList[8] == 'ai') return false;
+//   if (fieldsList[0] == 'ai' && fieldsList[3] == 'ai' && fieldsList[6] == 'ai') return false;
+//   if (fieldsList[1] == 'ai' && fieldsList[4] == 'ai' && fieldsList[7] == 'ai') return false;
+//   if (fieldsList[2] == 'ai' && fieldsList[5] == 'ai' && fieldsList[8] == 'ai') return false;
+//   if (fieldsList[0] == 'ai' && fieldsList[4] == 'ai' && fieldsList[8] == 'ai') return false;
+//   if (fieldsList[2] == 'ai' && fieldsList[4] == 'ai' && fieldsList[6] == 'ai') return false;
+//   return;
+// }
+//
+// function returnResult() {
+//   if (checkWin() === true) {
+//     alert( 'Вы выиграли');
+//   } else if (checkWin() === false){
+//     alert( 'Вы проиграли' );
+// }
+//
+// function reset() {
+//   returnResult();
+//   alert ('Игра окончена');
+//   location.reload();
+// }
 
 function gameOver(result) {
   switch (result) {
